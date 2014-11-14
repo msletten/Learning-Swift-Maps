f27d60fbdc8bab07b00e8bc28303efc75529df59
Mat Sletten <matsletten@mmintzmbp.bbbb.net>
2014-11-14 15:36:04 -0800

    Initial Commit

---
+++ Learning Swift Maps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learning Swift Maps
+//
+//  Created by Mat Sletten on 11/14/14.
+//  Copyright (c) 2014 Mat Sletten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
