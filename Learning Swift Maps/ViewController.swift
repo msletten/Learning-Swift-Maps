c9510e7d906ec0aeb6774484a266bc4e5d50f95a
Mat Sletten <matsletten@lyellenimac.bbbb.net>
2015-01-21 16:11:50 -0800

    updated for user actions

--- Learning Swift Maps/ViewController.swift
+++ Learning Swift Maps/ViewController.swift
@@ -8,15 +8,25 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
                             
     @IBOutlet weak var mapView: MKMapView!
+    
+    var userLocation = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //Core Location
+        userLocation.delegate = self
+        userLocation.desiredAccuracy = kCLLocationAccuracyBest
+        userLocation.requestWhenInUseAuthorization()
+        userLocation.startUpdatingLocation()
+        
+        //Map View
         //Devil's Tower coords: 44.590412, -104.714606
         var latitudeDT:CLLocationDegrees = 44.590412
         var longitudeDT:CLLocationDegrees = -104.714606
@@ -53,6 +63,26 @@ class ViewController: UIViewController, MKMapViewDelegate {
         touchAnnotation.subtitle = "Look, I touched my self"
         mapView.addAnnotation(touchAnnotation)
         
+    }
+    
+    func locationManager(userLocation: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var userNewLocation:CLLocation = locations[0] as CLLocation
+        var userLatitudeDT:CLLocationDegrees = userNewLocation.coordinate.latitude
+        var userLongitudeDT:CLLocationDegrees = userNewLocation.coordinate.longitude
+        //The bigger the xDelta(zoom level) is the more zoomed out the map will be. Essentially, we're setting a variable to be the value of the difference in the degrees from the left side of the screen to the right side of the screen
+        var userLatDelta:CLLocationDegrees = 0.01
+        var userLongDelta:CLLocationDegrees = 0.01
+        //This makes a geo span using set up values to be applied to the MKRegion we make. This in conjunction with the location we make will constitute the region.
+        var userMapSpan:MKCoordinateSpan = MKCoordinateSpanMake(userLatDelta, userLongDelta)
+        var userMapLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(userLatitudeDT, userLongitudeDT)
+        var userMapRegion:MKCoordinateRegion = MKCoordinateRegionMake(userMapLocation, userMapSpan)
+        
+        mapView.setRegion(userMapRegion, animated: true)
+//        println(userNewLocation.coordinate.latitude)
+    }
+    
+    func locationManager(userLocation: CLLocationManager!, didFailWithError error: NSError!) {
+        println(error)
     }
 
     override func didReceiveMemoryWarning() {
